203c9d8c320cc6a7be3eef3313c25ce5b88f7aa7
Mike Doel <mike@mikedoel.com>
2013-07-17 09:47:00 -0400

    migration and schema got out of sync

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130710190718) do
+ActiveRecord::Schema.define(:version => 20130603201330) do
 
   create_table "allocations", :force => true do |t|
     t.date     "start_date"
