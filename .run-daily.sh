e383467f1db973cf1f011ed0c3bad8710d5c421b
Ken Barker <ken@theedgecase.com>
2013-07-30 09:49:39 -0400

    Correct sed suppression of rake feedback

--- .run-daily.sh
+++ .run-daily.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rake db:transfer_prod_db
 heroku run rake utilization_today -a t2api > doc/daily
-sed '/DEPRECATION/d' ./doc/daily -i
+sed -i -e '/DEPRECATION/d' ./doc/daily
+sed -i -e '/Running/d' ./doc/daily
 cat doc/daily | pbcopy
 git diff
