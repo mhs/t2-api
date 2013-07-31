7c4fac476e796489a48115d5834f7dc1eba92348
Ken Barker <ken@theedgecase.com>
2013-07-31 10:46:56 -0400

    Clean-up sed droppings

--- .run-daily.sh
+++ .run-daily.sh
@@ -3,5 +3,6 @@ rake db:transfer_prod_db
 heroku run rake utilization_today -a t2api > doc/daily
 sed -i -e '/DEPRECATION/d' ./doc/daily
 sed -i -e '/Running/d' ./doc/daily
+rm doc/daily-e
 cat doc/daily | pbcopy
 git diff
