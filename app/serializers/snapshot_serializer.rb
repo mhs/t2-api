372d2fc7cc730dd3f1e38c96ab34bd497d03d2a0
Mike Doel <mike@mikedoel.com>
2013-08-06 10:42:03 -0400

    properly embed people in snapshot serialization

--- app/serializers/snapshot_serializer.rb
+++ app/serializers/snapshot_serializer.rb
@@ -1,5 +1,5 @@
 class SnapshotSerializer < ActiveModel::Serializer
-  attributes :id, :snap_date, :staff_ids, :overhead_ids, :billable_ids, :unassignable_ids, :assignable_ids, :billing_ids, :non_billing_ids, :utilization
+  attributes :id, :snap_date, :utilization, :staff_ids, :overhead_ids, :billable_ids, :unassignable_ids, :assignable_ids, :billing_ids, :non_billing_ids
+  embed :ids, include: true
   has_many :people
-  has_one :office, embed: :ids
 end
