b3cd15b1d842932af3088bd240322695ebd1ce1d
Mike Doel <mike@mikedoel.com>
2013-08-15 15:13:07 -0400

    redirect properly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
 
-  def after_sign_in_path_for(resource)
-    session[:return_url] || 'http://t2.neo.com'
+  def after_sign_in_path_for(user)
+    # Holy fuck
+    url = URI.parse(session[:return_url] || 'http://t2.neo.com')
+    token = [['token',user.authentication_token]]
+    url.query = URI.encode_www_form(URI.decode_www_form(url.query || "") + token)
+    url.to_s
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,4 +5,5 @@ puts 'No GOOGLE_CLIENT_ID and/or GOOGLE_SECRET specified in ENV' if [ENV['GOOGLE
 Devise.setup do |config|
   require 'omniauth-google-oauth2'
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET'], { access_type: "offline", approval_prompt: "" }
+  config.token_authentication_key = :authentication_token
 end
