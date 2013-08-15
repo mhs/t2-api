9f42cf5787f4e30c17196558ce7e60f1e3a40648
Mike Doel <mike@mikedoel.com>
2013-08-15 18:17:32 -0400

    copy the token from the header to params hash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+  prepend_before_filter :get_auth_token
 
   def after_sign_in_path_for(user)
     # Holy fuck
@@ -9,4 +10,14 @@ class ApplicationController < ActionController::Base
     url.query = URI.encode_www_form(URI.decode_www_form(url.query || "") + token)
     url.to_s
   end
+
+  private
+      def get_auth_token
+          if auth_token = params[:authentication_token].blank? && request.headers["Authorization"]
+              # we're overloading ActiveResource's Basic HTTP authentication here, so we need to
+              # do some unpacking of the auth token and re-save it as a parameter.
+              params[:authentication_token] = auth_token
+          end
+      end
+
 end
