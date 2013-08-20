b6bddf47e5cbfc3106286aabc719f5a57f369e36
swalke16 <swalke16@gmail.com>
2013-08-20 13:49:40 -0400

    fixed some bugs where the save was no going through properly due to missing person id

--- app/serializers/pto_request_serializer.rb
+++ app/serializers/pto_request_serializer.rb
@@ -1,4 +1,4 @@
 class PtoRequestSerializer < ActiveModel::Serializer
-  attributes :id, :notes, :start_date, :end_date, :project_name, :project_id
+  attributes :id, :notes, :start_date, :end_date, :project_name, :project_id, :person_id
 end
 
