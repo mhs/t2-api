cd2474469e155b48dd49afa69a83f12b5aa38bd0
Stuart Nelson + Scott Walker <pairing+sn+sw@edgecase.com>
2013-09-03 15:28:11 -0400

    add migration and people linking tasks to daily rake task

--- .run-daily.sh
+++ .run-daily.sh
@@ -5,4 +5,8 @@ sed -i -e '/DEPRECATION/d' ./doc/daily
 sed -i -e '/Running/d' ./doc/daily
 rm doc/daily-e
 cat doc/daily | pbcopy
+rake db:migrate:up VERSION=20130821193648
+rake db:migrate:up VERSION=20130823175015
+rake db:migrate:up VERSION=20130823182509
+rake db:link_people_to_users
 git diff
