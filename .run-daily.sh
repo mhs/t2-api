f07cdab04acdb4df3ff4042ccae7b27ff9c9f049
Mike Doel <mike@mikedoel.com>
2013-09-04 09:38:23 -0400

    Update run-daily script to apply correct migrations on heroku

--- .run-daily.sh
+++ .run-daily.sh
@@ -5,8 +5,7 @@ sed -i -e '/DEPRECATION/d' ./doc/daily
 sed -i -e '/Running/d' ./doc/daily
 rm doc/daily-e
 cat doc/daily | pbcopy
-rake db:migrate:up VERSION=20130821193648
-rake db:migrate:up VERSION=20130823175015
-rake db:migrate:up VERSION=20130823182509
-rake db:link_people_to_users
+heroku run rake db:migrate:up VERSION=20130821193648 -a t2api
+heroku run rake db:migrate:up VERSION=20130823175015 -a t2api
+heroku run rake db:link_people_to_users -a t2api
 git diff
