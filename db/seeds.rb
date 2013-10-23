aa9a0dc7e1f8c975e4220babdfd8790683d23168
Alvaro Gil <zevarito@gmail.com>
2013-10-23 10:35:55 -0200

    update dev port for people app again

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ applications_attributes = [
   {url: "http://localhost:7000",            icon: "g", title: "Utilization"},
   {url: "http://brockman.herokuapp.com",    icon: "b", title: "Pipeline"},
   {url: "http://localhost:8000",            icon: "p", title: "PTO"},
-  {url: "http://localhost:6000",            icon: "u", title: "Profile"},
+  {url: "http://localhost:9999",            icon: "u", title: "Profile"},
   {url: "http://localhost:8888",            icon: "s", title: "Settings"}
 ]
 applications_attributes.each_with_index do |attrs, index|
