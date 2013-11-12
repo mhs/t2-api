54ddec93e1d0397e0854f238708a2b02f4317896
Matt Jones + Chris Westra <pairing+mj+cw@edgecase.com>
2013-11-12 18:53:25 -0500

    backport an encoding fix from Rails 4 to handle astral plane characters

--- config/initializers/duck_punches.rb
+++ config/initializers/duck_punches.rb
@@ -11,3 +11,25 @@ class Date
     saturday? || sunday?
   end
 end
+
+
+#
+# Backport fix for https://github.com/rails/rails/issues/3727
+# Was reverted from 3-2-stable because it caused breaking changes
+# Unfucks astral plane characters
+#
+module ActiveSupport
+  module JSON
+    module Encoding
+      class << self
+        def escape(string)
+          string = string.encode(::Encoding::UTF_8, :undef => :replace).force_encoding(::Encoding::BINARY)
+          json = string.gsub(escape_regex) { |s| ESCAPED_CHARS[s] }
+          json = %("#{json}")
+          json.force_encoding(::Encoding::UTF_8)
+          json
+        end
+      end
+    end
+  end
+end
