6cd61dd5b0c892c9ec5eb9943f44be71e754f386
= <=>
2013-11-15 16:19:26 -0500

    Return utilizations by office.

--- app/models/utilization_summary.rb
+++ app/models/utilization_summary.rb
@@ -1,11 +1,19 @@
 class UtilizationSummary
-  attr_reader :id, :office_id, :office_name, :office_slug, :utilization_counts, :snapshot
+  attr_reader :id, :office_id, :office_name, :office_slug, :utilization_counts, :snapshot, :by_office_utilizations
   def initialize(params={})
     @office_id = params[:office_id] || nil
     snap_date = params[:snap_date] || Date.today
     summary_start_date = params[:summary_start_date] || snap_date - 1.week
     summary_end_date = params[:summary_end_date] || snap_date + 1.week
     @snapshot = Snapshot.on_date!(snap_date, @office_id)
+    context_snapshots = Office.all.map do |office|
+      Snapshot.on_date!(snap_date, office.id)
+    end
+
+    @by_office_utilizations = context_snapshots.map do |snap|
+      {name: snap.office.name, utilization: snap.utilization.to_f}
+    end
+
     @utilization_counts = UtilizationCount.for_weekdays_between(summary_start_date, summary_end_date, office_id)
   end
 
--- app/serializers/utilization_summary_serializer.rb
+++ app/serializers/utilization_summary_serializer.rb
@@ -1,5 +1,5 @@
 class UtilizationSummarySerializer < ActiveModel::Serializer
-  attributes :id, :office_id, :office_name, :office_slug
+  attributes :id, :office_id, :office_name, :office_slug, :by_office_utilizations
 
   has_many :utilization_counts, embed: :ids, include: true
   has_one :snapshot, embed: :ids, include: true
