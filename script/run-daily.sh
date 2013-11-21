6727ec7a8a4895088c663d3ffcdcca27c2a83ad8
Mike Doel <mike@mikedoel.com>
2013-11-21 14:30:37 -0500

    add migrations and cleanup to daily task

--- script/run-daily.sh
+++ script/run-daily.sh
@@ -10,7 +10,9 @@ heroku run rake db:migrate:up VERSION=20130823175015 -a t2api
 heroku run rake db:migrate:up VERSION=20130904145751 -a t2api
 heroku run rake db:migrate:up VERSION=20131015175419 -a t2api
 heroku run rake db:migrate:up VERSION=20131101135609 -a t2api
+heroku run rake db:migrate:up VERSION=20131120221416 -a t2api
 heroku run rake db:link_people_to_users -a t2api
 heroku run rake db:clean_users_and_people -a t2api
 heroku run rake applications:set_default_for_all -a t2api
+heroku run rake db:restore_deleted_from_snapshots -a t2api
 git diff
