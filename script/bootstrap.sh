3d39ef493d5e446f63deb9bebef1430a17fe12ac
Fernando Briano <fernando@picandocodigo.net>
2013-12-09 20:24:02 -0200

    Adds port forwarding on Vagrant box Changes versions in Gemfile, update Gemfile.lock Changes in script on Postgres for UTF-8

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -23,10 +23,17 @@ echo "export LANG=en_US.UTF-8" >> /etc/bash.bashrc
 locale-gen en_US.UTF-8
 dpkg-reconfigure locales
 
+su - postgres -c "psql -c \"update pg_database set datistemplate=false where datname='template1';\""
+su - postgres -c "psql -c 'drop database Template1;'"
+su - postgres -c "psql -c \"create database template1 with owner=postgres encoding='UTF-8' lc_collate='en_US.utf8' lc_ctype='en_US.utf8' template template0;\""
+su - postgres -c "psql -c \"update pg_database set datistemplate=true where datname='template1';\""
+
 su - postgres -c "psql -c 'CREATE USER vagrant WITH CREATEDB CREATEROLE;'"
 
 sed -i 's/md5/trust/g' /etc/postgresql/9.1/main/pg_hba.conf
 sed -i 's/#listen/listen/g' /etc/postgresql/9.1/main/postgresql.conf
+
+/etc/init.d/postgresql restart
 
 # T2 stuff starts here:
 if [ ! -f /vagrant/.env ]; then
@@ -37,19 +44,27 @@ if [ ! -f /vagrant/config/database.yml ]; then
     su - vagrant -c 'cp /vagrant/config/database{.sample,}.yml'
 fi
 
-if grep -i "GOOGLE_CLIENT_ID" ~/.bashrc; then
+if grep -i "GOOGLE_CLIENT_ID" /home/vagrant/.bashrc; then
     echo "Google Client ID already set"
 else
     su - vagrant -c 'echo "export GOOGLE_CLIENT_ID=\"480140443980.apps.googleusercontent.com\"" >> ~/.bashrc'
+    su - vagrant -c 'echo "GOOGLE_CLIENT_ID=\"480140443980.apps.googleusercontent.com\"" >> /vagrant/.env'
 fi
 
-if grep -i "GOOGLE_CLIENT_ID" ~/.bashrc; then
+if grep -i "GOOGLE_SECRET" /home/vagrant/.bashrc; then
     echo "Google Secret already set"
 else
     su - vagrant -c 'echo "export GOOGLE_SECRET=\"v03dRPvKlgn_OsK79MXSDn5j\"" >>  ~/.bashrc'
+    su - vagrant -c 'echo "GOOGLE_SECRET=\"v03dRPvKlgn_OsK79MXSDn5j\"" >>  /vagrant/.env'
 fi
+
 su - vagrant -c 'source ~/.bashrc'
 
+if grep -i "RAILS_SECRET_KEY_BASE" /vagrant/.env; then
+    echo "Secret key set"
+else
+    su - vagrant -c 'echo RAILS_SECRET_KEY_BASE=`cd /vagrant && rake secret` > /vagrant/.env'
+fi
 
 if [ -f /vagrant/Gemfile.lock ]; then
   rm /vagrant/Gemfile.lock
