fe864b918cfa9a3131c3cbc62a90b718f3b7a0fc
Matt Jones + Chris Westra <pairing+mj+cw@edgecase.com>
2013-12-16 15:54:45 -0500

    Validate people's roles.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,17 @@
 class Person < ActiveRecord::Base
+
+  ROLES = [
+    'Apprentice',
+    'Business Development',
+    'Designer',
+    'Developer',
+    'General & Administrative',
+    'Managing Director',
+    'Principal',
+    'Product Manager',
+    'Support Staff'
+  ]
+
   acts_as_paranoid
   acts_as_taggable_on :skills
 
@@ -19,6 +32,8 @@ class Person < ActiveRecord::Base
   validates :office, presence: true
   validates :email, presence: true, uniqueness: true
   validates :user_id, presence: true, on: :update
+
+  validates :role, inclusion: { in: ROLES }
 
   scope :employed_on_date, lambda { |d|
     where("start_date is NULL or start_date < ?",d)
