22bbd4f1dfb71acd2a3acd48701715acd48edeed
Matt Jones + Chris Westra <pairing+mj+cw@edgecase.com>
2014-01-03 14:46:07 -0500

    Update schema.

--- db/schema.rb
+++ db/schema.rb
@@ -44,21 +44,6 @@ ActiveRecord::Schema.define(version: 20140102220403) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "companies", force: true do |t|
-    t.text     "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "companies", ["name"], name: "index_companies_on_name", unique: true, using: :btree
-
-  create_table "contacts", force: true do |t|
-    t.string  "name"
-    t.string  "email"
-    t.string  "phone"
-    t.integer "company_id"
-  end
-
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0
     t.integer  "attempts",   default: 0
@@ -95,26 +80,6 @@ ActiveRecord::Schema.define(version: 20140102220403) do
     t.string   "slug"
     t.datetime "deleted_at"
     t.integer  "position"
-  end
-
-  create_table "opportunities", force: true do |t|
-    t.string   "title"
-    t.string   "stage"
-    t.string   "confidence",          default: "warm"
-    t.decimal  "amount",              default: 0.0
-    t.datetime "expected_date_close"
-    t.integer  "person_id",                            null: false
-    t.integer  "company_id"
-    t.integer  "contact_id"
-    t.text     "description"
-  end
-
-  create_table "opportunity_notes", force: true do |t|
-    t.text     "detail",         null: false
-    t.integer  "person_id",      null: false
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.integer  "opportunity_id"
   end
 
   create_table "people", force: true do |t|
