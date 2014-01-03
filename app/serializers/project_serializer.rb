304ef0b88109b4158478f42c71a2f3da0b1ea5ac
Matt Jones + Chris Westra <pairing+mj+cw@edgecase.com>
2014-01-03 16:23:04 -0500

    Project offices can now be set.

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,7 +1,7 @@
 class ProjectSerializer < ActiveModel::Serializer
   embed :ids
 
-  attributes :id, :name, :notes, :billable, :vacation
+  attributes :id, :name, :notes, :billable, :vacation, :start_date, :end_date
   # has_many :slots
   has_many :offices, embed: :ids
   has_many :allocations, embed: :ids
