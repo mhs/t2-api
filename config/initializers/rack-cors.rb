c680ba8cd33d8aeb6a9b5490a0b5167125005581
Matt Jones <al2o3cr@gmail.com>
2014-01-06 14:02:18 -0500

    attempt to fix CORS behavior

--- config/initializers/rack-cors.rb
+++ config/initializers/rack-cors.rb
@@ -5,6 +5,7 @@ Rails.application.config.middleware.insert_before Warden::Manager, Rack::Cors do
     resource '*',
             #headers: ['Origin', 'Accept', 'Content-Type', 'X-CSRF-Token'],
             headers: :any,
-            methods: [:get, :post, :put, :delete, :options]
+            methods: [:get, :post, :put, :delete, :options],
+            credentials: false
   end
 end
