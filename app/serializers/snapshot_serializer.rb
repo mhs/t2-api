0c6b47bb36f9c6d6959b451b2bbb2613e9abd0c5
Matt Jones + Chris Westra <pairing+mj+cw@edgecase.com>
2014-01-16 08:59:31 -0500

    Change snapshot attr ids to weights.
    Remove billable and overhead weights, as this information is now
    included in the staff_weights.

--- app/serializers/snapshot_serializer.rb
+++ app/serializers/snapshot_serializer.rb
@@ -1,6 +1,6 @@
 class SnapshotSerializer < ActiveModel::Serializer
-  attributes :id, :snap_date, :utilization, :staff_ids, :overhead_ids, :billable_ids,
-    :unassignable_ids, :assignable_ids, :billing_ids, :non_billing_ids, :office_id
+  attributes :id, :snap_date, :utilization, :staff_weights,
+    :unassignable_weights, :assignable_weights, :billing_weights, :non_billing_weights, :office_id
 
   embed :ids, include: true
 
