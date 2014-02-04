6d4ec41c644ee726db1f3c9943bcbcefc73a151f
Jaime Andres <andresdavila6@gmail.com>
2014-02-04 17:20:54 -0200

    updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -48,8 +48,8 @@ ActiveRecord::Schema.define(version: 20140204152927) do
 
   create_table "companies", force: true do |t|
     t.text     "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "companies", ["name"], name: "index_companies_on_name", unique: true, using: :btree
@@ -120,8 +120,8 @@ ActiveRecord::Schema.define(version: 20140204152927) do
   create_table "opportunity_notes", force: true do |t|
     t.text     "detail",         null: false
     t.integer  "person_id",      null: false
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "opportunity_id"
   end
 
