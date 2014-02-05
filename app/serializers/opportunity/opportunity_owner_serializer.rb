dcfdf68e3f5b7a6b70a254e76b03960f219ee62b
Jaime Andres <andresdavila6@gmail.com>
2014-02-05 17:31:21 -0200

    removes unnecessary code for opportunityserializer

--- app/serializers/opportunity/opportunity_owner_serializer.rb
+++ app/serializers/opportunity/opportunity_owner_serializer.rb
@@ -2,10 +2,6 @@ class Opportunity::OpportunityOwnerSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :role, :avatar
 
   def avatar
-    begin
-      object.avatar.url(:thumb)
-    rescue
-      ""
-    end
+    object.avatar.url(:thumb)
   end
 end
