88d21a276cb7d028f1b338b14a8db5137e6303cc
Alvaro Gil <zevarito@gmail.com>
2014-02-06 11:04:27 -0200

    fix order of opportunity notes

--- spec/serializers/opportunity_serializer_spec.rb
+++ spec/serializers/opportunity_serializer_spec.rb
@@ -9,4 +9,12 @@ describe Opportunity::OpportunitySerializer do
   it "should serialize with root element" do
     opportunity_serialized.as_json.keys.should eql([:opportunity])
   end
+
+  it "should return opportunity notes ordered by created_at desc" do
+    op_b = FactoryGirl.create(:opportunity_note, created_at: 2.days.ago, opportunity: opportunity).id
+    op_a = FactoryGirl.create(:opportunity_note, created_at: 3.days.ago, opportunity: opportunity).id
+    op_c = FactoryGirl.create(:opportunity_note, created_at: 1.days.ago, opportunity: opportunity).id
+
+    opportunity_serialized.as_json[:opportunity][:opportunity_notes].should eql([op_c,op_b,op_a])
+  end
 end
