d3a7c6b23c2c4e60415a3d128e24e387dc26832c
xandrews <john@neo.com>
2014-02-10 18:20:56 -0500

    Fix secret token config

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-T2Api::Application.config.secret_token = '8ec24829872178aaa5808d465b38576d587f7d41621a51dc52a626ba004e5040462e6e6bc82fcd2398f065ae52135ee2282e80a5b8a0951732cdbcae7c84578b'
+T2Api::Application.config.secret_key_base = '8ec24829872178aaa5808d465b38576d587f7d41621a51dc52a626ba004e5040462e6e6bc82fcd2398f065ae52135ee2282e80a5b8a0951732cdbcae7c84578b'
