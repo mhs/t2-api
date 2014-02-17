b78b3e1999e997d106fcafd9b781332d0ab9cd6c
Aaron Christy + Chandu Tennety <pairing+ac+ct@edgecase.com>
2014-02-17 16:28:58 -0500

    make a daily report class

--- app/mailers/utilization_mailer.rb
+++ app/mailers/utilization_mailer.rb
@@ -6,19 +6,7 @@ class UtilizationMailer < ActionMailer::Base
 
   def daily
     recipients = "aaron@neo.com"
-    offices = Office.reporting
-    @global = [Snapshot.today!]
-    @projected = []
-    with_week_days 21 do |date|
-      @projected << Snapshot.on_date!(date)
-    end
-
-    @current_month = [MonthlySnapshot.today!]
-    @current_month += offices.map { |office| MonthlySnapshot.today!(office) }
-
-    @future_month = [MonthlySnapshot.next_month!]
-    @future_month += offices.map { |office| MonthlySnapshot.next_month!(office) }
-
+    @report = UtilizationDailyReport.new
     mail to: recipients, subject: "Daily Utilization", from: "T2 Utilization"
   end
 end
