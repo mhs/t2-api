a9ecf4918df14f3bcb9810ac550843782f4844ba
Aaron Christy + Chandu Tennety <pairing+ac+ct@edgecase.com>
2014-02-18 10:54:33 -0500

    Hook up mandrillapp mailer

--- app/mailers/utilization_mailer.rb
+++ app/mailers/utilization_mailer.rb
@@ -1,12 +1,20 @@
-require 'date_range_helper'
-
 class UtilizationMailer < ActionMailer::Base
   layout 'mailer'
-  include DateRangeHelper
 
-  def daily
-    recipients = "aaron@neo.com"
+  def daily(recipients="bot@neo.com")
     @report = UtilizationDailyReport.new
-    mail to: recipients, subject: "Daily Utilization", from: "T2 Utilization"
+    mail to: recipients, from: from("daily+utilization"), subject: subject("Daily Utilization")
+  end
+
+
+  private
+
+  def subject(s)
+    return s if Rails.env.production?
+    "[#{Rails.env}] #{s}"
+  end
+
+  def from(f)
+    "bot+#{f}@neo.com"
   end
 end
