5f888354c1e169813fa98520c4dd54842e042d78
Matt Jones + Tony Schneider <pairing+mj+ts@edgecase.com>
2014-05-06 16:56:33 -0400

    Add t2 projects to db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 # encoding: utf-8
 T2Application.delete_all
 applications_attributes = [
-  {url: "http://localhost:9000",            icon: "📊", title: "Allocations", classes: "allocations"},
-  {url: "http://localhost:7000",            icon: "📈", title: "Utilization"},
-  {url: "http://localhost:9999",            icon: "👤", title: "Neons"},
+  {url: "http://localhost:9000", icon: "📊", title: "Allocations", classes: "allocations"},
+  {url: "http://localhost:7000", icon: "📈", title: "Utilization"},
+  {url: "http://localhost:9999", icon: "👤", title: "Neons"},
+  {url: "http://localhost:9001", icon: "", title: "Projects"}
 ]
 applications_attributes.each_with_index do |attrs, index|
   T2Application.create(attrs.merge(position: index))
