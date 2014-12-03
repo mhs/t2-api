1e92b702ef7a43c86725205d6c95bce11d71ac94
Sara Gibbons <saragibby@gmail.com>
2014-12-03 16:26:32 -0500

    add deleted flag to serialized office data

--- app/serializers/office_serializer.rb
+++ app/serializers/office_serializer.rb
@@ -1,5 +1,5 @@
 class OfficeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :notes, :slug, :position
+  attributes :id, :name, :notes, :slug, :position, :deleted
 
   has_many :projects, embed: :ids
   has_many :people, embed: :ids
