d9abe8f71863c8a63a27f224342ac44f141aa126
Sara Gibbons + Ryan Stocker + Chris Westra <saragibby+sg+rs+cw@gmail.com>
2015-01-05 11:26:46 -0500

    make sure that vacation allocations do not exceed 100 percent

---
+++ spec/models/utilization_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Utilization do
+
+  describe '#vacation_allocation_percentage' do
+    it 'aint nobody get more then 100% allocation to vacation on the same day' do
+      person = FactoryGirl.create(:person)
+      project = FactoryGirl.create(:project, :vacation)
+      2.times do
+        FactoryGirl.create(:allocation, :vacation, person: person, project: project)
+      end
+
+      Utilization.new(person: person, includes_provisional: false).vacation_allocation_percentage.should eq(100.0)
+    end
+  end
+
+end
