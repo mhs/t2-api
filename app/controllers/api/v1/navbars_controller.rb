e3a4f243afafc5e8dc2f0d5d462d39fd7c567f4b
Sara Gibbons + Ryan Stocker + Chris Westra <saragibby+sg+rs+cw@gmail.com>
2015-01-14 14:29:43 -0500

    Cache navbar json.

--- app/controllers/api/v1/navbars_controller.rb
+++ app/controllers/api/v1/navbars_controller.rb
@@ -1,8 +1,11 @@
 class Api::V1::NavbarsController < Api::V1::BaseController
 
   def show
-    navbar = Navbar.new(T2Application.order(:position).to_a)
+    key = T2Application.maximum(:updated_at)
+    navbar_json = Rails.cache.fetch key do
+      Navbar.new(T2Application.order(:position).to_a).to_json
+    end
 
-    render json: navbar, root: false
+    render json: navbar_json, root: false
   end
 end
